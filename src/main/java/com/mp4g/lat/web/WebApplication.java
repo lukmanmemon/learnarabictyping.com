a29a05008f422a847e7a732d763275fd7994a6e9
Mubariz Afzal <mubarizafzal@gmail.com>
2021-05-16 08:01:30 -0400

    first commit

---
+++ src/main/java/com/mp4g/lat/web/WebApplication.java
@@ -0,0 +1,13 @@
+package com.mp4g.lat.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
